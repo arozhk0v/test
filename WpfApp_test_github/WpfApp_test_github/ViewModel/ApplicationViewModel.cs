f687bc6ee15c4c0954426dbd4231f9bf21a9073d
Alex_Fallen_Angel <AlexMrHam@ya.ru>
2016-11-09 21:50:15 +0300

    add files

---
+++ WpfApp_test_github/WpfApp_test_github/ViewModel/ApplicationViewModel.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using WpfApp_test_github.Model;
+
+namespace WpfApp_test_github
+{
+    /// <summary>
+    /// View Model
+    /// </summary>
+    class ApplicationViewModel : INotifyPropertyChanged
+    {
+        private Person person;
+        public ObservableCollection<Person> Persons { get; set; }
+
+        public ApplicationViewModel()
+        {
+            Persons = new ObservableCollection<Person>
+            {
+                new Person { Name = "Григорий" },
+                new Person { Name = "Сергей" },
+                new Person { Name = "Петр" }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName]string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+
+    }
+}
